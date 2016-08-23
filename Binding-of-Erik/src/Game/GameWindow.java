b100651b81cdddeb8fcd61edc2896a797563223d
eriksandberg <e.sandberg92@gmail.com>
2016-08-23 12:05:58 +0200

    Cleaned up some old code.

--- Binding-of-Erik/src/Game/GameWindow.java
+++ Binding-of-Erik/src/Game/GameWindow.java
@@ -11,43 +11,26 @@ import javax.swing.*;
 import java.awt.EventQueue;
 import javax.swing.JFrame;
 
-
-
 public class GameWindow extends JFrame {
 
     private static final int FRAMEWIDTH = 800; //pixels, also our entire coordinates system.
     private static final int FRAMEHEIGHT = 800;
 
-    public GameWindow() {
-        windowInit();
-    }
-
-    // Launching the graphical interface (name frameInit is taken)
-    private void windowInit() { //haven't gotten this to work yet
-
-        // As of now we'll just create one room, swaping rooms come later
-        //add(new Room());
-
-        // Set frame name to 'Binding of Erik', set size of frame, center frame on the screen,
-        // set default to close the application when the window is closed and
-        // make the window visiable
-
-	/*GameFrame StartingRoom = new GameFrame(startingRoom, "Binding of Erik");
-        setTitle("Binding of Erik");
-        setSize(400, 400);
-        setLocationRelativeTo(null);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);*/
-    }
-
     // Main
     public static void main(String[] args) {
 
-	System.out.println("Binding of Erik: Running!");
-	Room startingRoom = new Room(FRAMEWIDTH, FRAMEHEIGHT);
-	GameFrame window = new GameFrame(startingRoom, "Binding of Erik");
-        //GameWindow window = new GameWindow(); //haven't gotten the init to work yet
-	window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.pack();
-        window.setVisible(true);
+		// Set frame name to 'Binding of Erik', set size of frame,
+		// set default to close the application when the window is closed,
+		// center the frame on the screen and make the window visiable
+		Room startingRoom = new Room(FRAMEWIDTH, FRAMEHEIGHT);
+		GameFrame window = new GameFrame(startingRoom, "Binding of Erik");
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.pack();
+		window.setLocationRelativeTo(null);
+		window.setVisible(true);
+		System.out.println("Binding of Erik: Running!");
+
+		//window.setVisible(false);
+		//window.dispose();
     }
 }
