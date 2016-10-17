fc395aefd887ecaa2a965bf1d237fb953efdc238
danwa223 <d.wassing@gmail.com>
2016-10-17 11:29:25 +0200

    See previous commit.

--- Binding-of-Erik/src/binding_of_erik_game/Galaxy.java
+++ Binding-of-Erik/src/binding_of_erik_game/Galaxy.java
@@ -3,6 +3,9 @@ package binding_of_erik_game;
 /**
  * Created by wassing on 2016-10-17.
  */
-public class Galaxy
-{
+public class Galaxy extends GameObject{
+
+	public Galaxy(TileType[][] shape) {
+		super(shape);
+	}
 }
