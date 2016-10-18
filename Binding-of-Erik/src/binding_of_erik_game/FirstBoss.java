597fa49971021b9d05a36c78687ce79b37e3887e
danwa223 <d.wassing@gmail.com>
2016-10-18 11:26:14 +0200

    Added a stardestroyer sprite as first boss. Mechanics still missing, behaves like normal enemy right now.

--- Binding-of-Erik/src/binding_of_erik_game/FirstBoss.java
+++ Binding-of-Erik/src/binding_of_erik_game/FirstBoss.java
@@ -3,6 +3,17 @@ package binding_of_erik_game;
 /**
  * Created by wassing on 2016-10-18.
  */
-public class FirstBoss
+public class FirstBoss extends Enemy
 {
+	public FirstBoss(TileType[][] shape) {
+		super(shape);
+	    	this.size = 18;
+		this.isEnemy = true;	// Born evil.
+		this.yCoord = 0;        // All enemies spawn at the top of the room
+	    	this.hp = HP;
+	    	this.worth = WORTH;
+	}
+
+    	public static final int WORTH = 5000;
+    	private static final int HP = 50;
 }
