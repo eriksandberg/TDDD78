565a1a5b9677c1ddbea426390d5f72077d1d0645
eriksandberg <e.sandberg92@gmail.com>
2016-10-18 18:00:10 +0200

    IDEA complained about room not being declared final.

--- Binding-of-Erik/src/binding_of_erik_game/GameFrame.java
+++ Binding-of-Erik/src/binding_of_erik_game/GameFrame.java
@@ -10,35 +10,34 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import javax.swing.JFrame;
 
-public class GameFrame extends JFrame{ //this class does all the actual frame updating work. Will be instanced as an object for GameWindow.
+public class GameFrame extends JFrame { //this class does all the actual frame updating work. Will be instanced as an object for GameWindow.
 
-    private final Room room;
-    private static boolean paused = false;
+	private final Room room;
+	private static boolean paused = false;
 
-    public GameFrame(Room room, String myWindowTitle){
- 	super(myWindowTitle);
+	public GameFrame(final Room room, String myWindowTitle) {
+		super(myWindowTitle);
 
-	this.room = room;
-	EventHandler paintArea = new EventHandler(room);
-	this.setLayout(new BorderLayout());
-	this.add(paintArea, BorderLayout.CENTER);
-	//this.createMenu(); extra for later, options for file etc.
+		this.room = room;
+		EventHandler paintArea = new EventHandler(room);
+		this.setLayout(new BorderLayout());
+		this.add(paintArea, BorderLayout.CENTER);
+		//this.createMenu(); extra for later, options for file etc.
 
-	final Action doOneStep = new AbstractAction()
-	{
-	    public void actionPerformed(ActionEvent e) {
-		if (!paused) {  // Since player movement is currently done outside of tick() this is not really a true pause
-		    room.tick();
-		}
-	    }
-	};
-	Timer clockTimer = new Timer(100, doOneStep); //10 ticks per second atm, will increase framerate to about 30 later on.
-	clockTimer.setCoalesce(true);
-	clockTimer.start();
-    }
+		final Action doOneStep = new AbstractAction() {
+			public void actionPerformed(ActionEvent e) {
+				if (!paused) {  // Since player movement is currently done outside of tick() this is not really a true pause
+					room.tick();
+				}
+			}
+		};
+		Timer clockTimer = new Timer(100, doOneStep); //10 ticks per second atm, will increase framerate to about 30 later on.
+		clockTimer.setCoalesce(true);
+		clockTimer.start();
+	}
 
-    // Pause the game
-    public static void togglePaused() {
+	// Pause the game
+	public static void togglePaused() {
 		paused = !paused;
 	}
 }
