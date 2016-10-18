b28f0cc36e6a22117cd5a617f88c80b415c8814c
eriksandberg <e.sandberg92@gmail.com>
2016-10-18 23:49:58 +0200

    Refactored DefaultCollisionHandler according to 5.18.1 in the lab guide.

--- testingIdea/src/tetris/DefaultCollisionHandler.java
+++ testingIdea/src/tetris/DefaultCollisionHandler.java
@@ -9,24 +9,20 @@ public class DefaultCollisionHandler implements CollisionHandler {
 
 		Poly poly = board.getPoly();
 
-		SquareType[][] squareArray = board.getSquareArray();
-
-		if (poly == null){
+		if (poly == null) {
 			return false;
 		}
-
 		//Don't have to check for X, only for Y. X only hinders side-movement, Y is the final determinator if we can place the block or not.
-		for (int i = 0; i < poly.getWidth(); i++){      // Loop over width of a poly
-			for (int j = 0; j < poly.getHeight(); j++){     // Loop over height
+		for (int i = 0; i < poly.getWidth(); i++) {      // Loop over width of a poly
+			for (int j = 0; j < poly.getHeight(); j++) {     // Loop over height
 				if ((poly.getShape()[i][j] != SquareType.EMPTY) &&  // Our current j + i combo is not empty space
-
-								(board.getTetrisPieceX()+i >= board.getColumns() ||     // Utanför höger kant
-								board.getTetrisPieceY()+j >= board.getRows() ||         // Över taket
-								board.getTetrisPieceX()+i < 0 ||                        // Är vi utanför vänster kant
-								board.getTetrisPieceY()+j < 0))                         // Under golvet
+						(board.getTetrisPieceX() + i >= board.getColumns() ||     // Utanför höger kant
+						 board.getTetrisPieceY() + j >= board.getRows() ||         // Över taket
+						 board.getTetrisPieceX() + i < 0 ||                        // Är vi utanför vänster kant
+						 board.getTetrisPieceY() + j < 0))                         // Under golvet
 					return true;
 				else if (poly.getShape()[i][j] != SquareType.EMPTY &&
-						squareArray[(i+board.getTetrisPieceX())][(j+board.getTetrisPieceY())] != SquareType.EMPTY){     //???
+						board.getSquareType(i, j) != SquareType.EMPTY) {     // i and j get added to
 					return true;
 				}
 			}
